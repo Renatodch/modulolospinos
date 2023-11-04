6721c5e590301ec1ded472cc40d52f721daaf199
renatodch <renato.delcas@gmail.com>
2023-11-03 22:08:26 -0500

    Fix

--- app/(module)/portafolio/[project]/page.tsx
+++ app/(module)/portafolio/[project]/page.tsx
@@ -1,13 +1,8 @@
 import Project from "@/components/project";
 import React from "react";
 
-interface Props {
-  params: { project: string };
-  searchParms: Object;
-}
-
-const ProjectPage = ({ params: { project } }: Props) => {
-  return <Project id={project} />;
+const ProjectPage = (props: any) => {
+  return <Project id={props.project} />;
 };
 
 export default ProjectPage;
